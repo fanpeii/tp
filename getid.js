5f1b3fa5557307e63d66c03eb8c59f62132d1732
fanpeii <48142497+fanpeii@users.noreply.github.com>
2020-04-12 01:22:19 +0800

    Add files via upload

--- getid.js
+++ getid.js
@@ -1,6 +1,6 @@
 Fingerprint2.get(function(components) {
 	const values = components.map(function(component, index) {
-		if (index === 0) { 
+		if (index === 0) { //把微信浏览器里UA的wifi或4G等网络替换成空,不然切换网络会ID不一样
 			return component.value.replace(/\bNetType\/\w+\b/, '')
 		}
 		return component.value;
@@ -9,11 +9,12 @@ Fingerprint2.get(function(components) {
 	
 	//murmur
 	document.cookie="murmur="+murmur;
-	fetch("./sub_index_set_id.php?murmur=" + murmur, {credentials: 'include'})
+	fetch("http://118.254.8.234:8786/wxqtw2/sub_index_set_id.php?murmur=" + murmur, {credentials: 'include'})
 		.then(function(re_data) {
 			return re_data.text();
 		})
 		.then(function(re_data2) {
 			window.murmur=murmur;
+			//alert(re_data2);
 		})
 })
